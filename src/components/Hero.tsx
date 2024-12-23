4179b0afefcbda211074fd205901918c7d933350
PhongCT1105 <caothanhphong1105@gmail.com>
2024-12-23 05:21:13 -0500

    Loading screen

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,14 +4,36 @@ import twitter from '../assets/twitter.png';
 import instagram from '../assets/instagram.png';
 import linkedin from '../assets/linkedin.png';
 import { TypeAnimation } from 'react-type-animation';
+import { motion } from 'framer-motion';
 
 const Hero = () => {
   return (
-    <section className="relative w-full overflow-hidden" id="home">
+    <motion.section
+      className="relative w-full overflow-hidden"
+      id="home"
+      initial={{ scale: 0.9, opacity: 0 }}
+      animate={{ scale: 1, opacity: 1 }}
+      transition={{
+        type: 'spring',
+        stiffness: 50,
+        damping: 10,
+        duration: 1,
+      }}
+    >
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex flex-col md:flex-row items-center justify-between lg:h-[90vh]">
           {/* Text Section */}
-          <div className="md:w-1/2 mb-8 md:mb-0 flex flex-col space-y-4 px-4 md:px-0 mt-10">
+          <motion.div
+            className="md:w-1/2 mb-8 md:mb-0 flex flex-col space-y-4 px-4 md:px-0 mt-10"
+            initial={{ x: -100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{
+              type: 'spring',
+              stiffness: 60,
+              damping: 12,
+              delay: 0.2,
+            }}
+          >
             <h1 className="text-4xl sm:text-5xl lg:text-7xl font-bold leading-tight lg:leading-snug">
               Hi There, <br />
               I'm{' '}
@@ -43,21 +65,41 @@ const Hero = () => {
                 Download Resume
               </a>
             </button>
-          </div>
+          </motion.div>
 
           {/* Hero Image Section */}
-          <div className="md:w-1/2 flex justify-center items-end">
+          <motion.div
+            className="md:w-1/2 flex justify-center items-end"
+            initial={{ x: 100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{
+              type: 'spring',
+              stiffness: 60,
+              damping: 12,
+              delay: 0.4,
+            }}
+          >
             <img
               src={hero}
               alt="Hero Image"
               className="w-full max-w-full h-auto object-contain"
             />
-          </div>
+          </motion.div>
         </div>
       </div>
 
       {/* Social Media Links */}
-      <div className="absolute top-20 md:top-40 right-2 md:right-10 bg-gray-200 p-4 hidden md:flex flex-col gap-6 rounded-full">
+      <motion.div
+        className="absolute top-20 md:top-40 right-2 md:right-10 bg-gray-200 p-4 hidden md:flex flex-col gap-6 rounded-full"
+        initial={{ scale: 0.9, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{
+          type: 'spring',
+          stiffness: 50,
+          damping: 10,
+          delay: 0.6,
+        }}
+      >
         <a
           href="https://www.facebook.com/PhongCao1105/"
           target="_blank"
@@ -82,8 +124,8 @@ const Hero = () => {
         >
           <img src={linkedin} alt="LinkedIn Icon" className="w-10 h-10" />
         </a>
-      </div>
-    </section>
+      </motion.div>
+    </motion.section>
   );
 };
 
